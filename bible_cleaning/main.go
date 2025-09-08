edbdc94d38446ba6c7f0f2b73a07aac7fe9e2b5e
Zhean Ganituen <zr.gntn@gmail.com>
2025-09-08 00:46:53 -0400

    Added complete bible for each language.
    Issue: Bibles for prf, rol, tao, tiu, tsg, msb, krj, cbk are incomplete

--- bible_cleaning/main.go
+++ bible_cleaning/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"regexp"
 	"sync"
 
@@ -8,7 +9,7 @@ import (
 )
 
 func main() {
-	total := 10
+	total := 1189
 
 	cleaningRes := []*regexp.Regexp{
 		regexp.MustCompile(`[^a-zA-Z0-9\s\.\,\;\:\!\?\'\"-]+`),
@@ -22,14 +23,14 @@ func main() {
 		"ceb": "https://www.bible.com/bible/562/GEN.1.RCPV",
 		"ilo": "https://www.bible.com/bible/782/GEN.1.RIPV",
 		"jil": "https://www.bible.com/bible/2190/GEN.1.MBBHIL12",
-		"bik": "https://www.bible.com/versions/890-mbbbik92-marahay-na-bareta-biblia",
+		"bik": "https://www.bible.com/bible/890/GEN.1.MBBBIK92",
 		"war": "https://www.bible.com/bible/2198/GEN.1.MBBSAM",
-		"pam": "https://www.bible.com/versions/1141-pmpv-ing-mayap-a-balita-biblia",
+		"pam": "https://www.bible.com/bible/1141/GEN.1.PMPV",
 		"pag": "https://www.bible.com/bible/2194/GEN.1.MBBPAN83",
 		"tiu": "https://www.bible.com/bible/2812/MAT.1.YBT",
-		"cbk": "https://www.bible.com/versions/1129-cbknt-el-nuevo-testamento",
+		"cbk": "https://www.bible.com/bible/1129/MAT.1.CBKNT",
 		"prf": "https://www.bible.com/bible/438/MAT.1.PRF",
-		"tsg": "https://www.bible.com/versions/1319-tsg-kitab-inji",
+		"tsg": "https://www.bible.com/bible/1319/MAT.1.TSG",
 		"rol": "https://www.bible.com/bible/2244/MAT.1.BKR",
 		"msb": "https://www.bible.com/bible/1222/MAT.1.MSB",
 		"krj": "https://www.bible.com/bible/1489/MAT.1.KRJNT",
@@ -37,11 +38,22 @@ func main() {
 	}
 	var wg sync.WaitGroup
 	for language, root := range bibles {
-		count := 0
+		count := 1
 		wg.Add(1)
 		go func(lang, url string, count *int) {
 			defer wg.Done()
-			scraper.ScrapeAndParse(root, language, "corpus/"+language, cleaningRes, make(map[string]bool), count, total)
+			fmt.Println(
+				language,
+				scraper.ScrapeAndParse(
+					root,
+					language,
+					"corpus/"+language,
+					cleaningRes,
+					make(map[string]bool),
+					count,
+					total,
+				),
+			)
 		}(language, root, &count)
 	}
 
