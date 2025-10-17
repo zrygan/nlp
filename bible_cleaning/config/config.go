4c86c233dd3520a8d89173f18267bc150485ff5d
Roan Cedric Campo <roan_campo@dlsu.edu.ph>
2025-10-17 14:00:05 +0800

    cleansing of original sin
    Our father, ForgedLives, thou art thy Neuron. Hallowed be his name. Thy minecraft come, thy wins be done. On the Overworld as it is in the End. Give us this day, our daily porkchop, and forgive our trespasses. As we forgive those who has hit the nae-nae. And lead us to not to Umamusume, but deliver us from the Fat one. Amen.
    
    Original has been purged.

--- bible_cleaning/config/config.go
+++ bible_cleaning/config/config.go
@@ -9,25 +9,12 @@ const (
 	PARALLEL_SENTENCES_FOLDER          = "parallel_corpus/by_sentences"
 	WORKER_REPORT_INTERVAL_MS          = 500 // milliseconds
 	THREAD_POOL_SIZE                   = 8   // number of worker threads
-<<<<<<< HEAD
-	IS_DETAILED                        = true
-=======
 	IS_DETAILED                        = false
->>>>>>> 7ecbd5fd670f8dfc71a1b8d24ef2f53232acf56c
 	USE_PROGRESS_BAR                   = true
 	WORKER_THREAD_REPORT_PROGRESS_RATE = 50 // report progress every N items processed
 )
 
 const (
-<<<<<<< HEAD
-	TOKEN_MISSING_TRANSLATION = "<MISSING_TRANSLATION>"
-	TOKEN_NEWLINE             = "<NEWLINE>"
-	TOKEN_SPACE               = "<SPACE>"
-	TOKEN_TAB                 = "<TAB>"
-	TOKEN_RETURN              = "<RETURN>"
-	DICE_SIMILARITY_THRESHOLD = 0.6
-	LENGTH_RATIO_BIAS         = 1.0 - DICE_SIMILARITY_THRESHOLD
-=======
 	TOKEN_MISSING_TRANSLATION    = "<MISSING_TRANSLATION>"
 	TOKEN_NEWLINE                = "<NEWLINE>"
 	TOKEN_SPACE                  = "<SPACE>"
@@ -36,5 +23,4 @@ const (
 	NGRAMS_DICE_SIMILARITY_BIAS  = 0.5
 	LENGTH_RATIO_SIMILARITY_BIAS = 0.3
 	PROPER_NOUNS_SIMILARITY_BIAS = 0.2
->>>>>>> 7ecbd5fd670f8dfc71a1b8d24ef2f53232acf56c
 )
