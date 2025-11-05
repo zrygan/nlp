ab0d908e82a97624c2cd3ad7dc7dcf913f934d64
Clarence Ang <clarence_ivan_ang@dlsu.edu.ph>
2025-11-06 01:16:11 +0800

    language cluster, there is also heatmap that you can use

---
+++ language_similarity/similaritymatrix/phonetic_heatmap.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load your TSV or CSV file
+# For TSV: sep='\t'
+df = pd.read_csv('phonetic_similarity_matrix.tsv', sep='\t', index_col=0)
+
+# Create a heatmap
+plt.figure(figsize=(8, 6))
+sns.heatmap(df, cmap='viridis', annot=False)
+
+plt.title('Phonetic Similarity Matrix')
+plt.xlabel('Language A')
+plt.ylabel('Language B')
+
+# Save as PNG or PDF for LaTeX
+plt.tight_layout()
+plt.savefig('phonetic_heat_map', dpi=300)
+plt.show()
