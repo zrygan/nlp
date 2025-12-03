83cd36253c9fa51d2c2a12e0f929fce7fa8e604f
Clarence Ang <clarence_ivan_ang@dlsu.edu.ph>
2025-12-04 01:42:07 +0800

    some writing

---
+++ nlp_tool/test/phonetics.py
@@ -0,0 +1,10 @@
+from metaphone import doublemetaphone 
+
+print(doublemetaphone('vancomycin'))
+print(doublemetaphone('triamcinolone'))
+print(doublemetaphone('psyllium'))
+print(doublemetaphone('tigecycline'))
+print(doublemetaphone('hydrochlorothiazide'))
+print(doublemetaphone('erythromycin'))
+print(doublemetaphone('doxycycline'))
+print(doublemetaphone('xylometazoline'))
